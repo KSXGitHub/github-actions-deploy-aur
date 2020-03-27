2fd0e71af2a923ccbf44b45b2915f86ae99ca88c
Gustavo Marin <gustavo.marin@intelygenz.com>
2020-03-27 18:20:32 +0100

    rename variables and capture from input

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,15 @@
 
 set -o errexit -o pipefail -o nounset
 
-REPO_NAME=$1
+PACKAGE_NAME=$INPUT_PACKAGE_NAME
+COMMIT_USERNAME=$INPUT_COMMIT_USERNAME
+COMMIT_EMAIL=$INPUT_COMMIT_EMAIL
+SSH_PRIVATE_KEY=$INPUT_SSH_PRIVATE_KEY
+
 NEW_RELEASE=${GITHUB_REF##*/v}
-GIT_USERNAME=$2
-GIT_EMAIL=$3
-SSH_PRIVATE_KEY=$4
 
 
-echo "---------------- AUR Package version $REPO_NAME/$NEW_RELEASE ----------------"
+echo "---------------- AUR Package version $PACKAGE_NAME/$NEW_RELEASE ----------------"
 
 ssh-keyscan -t ed25519 aur.archlinux.org >> ~/.ssh/known_hosts
 
@@ -17,17 +18,17 @@ echo -e "${SSH_PRIVATE_KEY//_/\\n}" > ~/.ssh/aur
 
 chmod 600 ~/.ssh/aur*
 
-git config --global user.name "$GIT_USERNAME"
-git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$COMMIT_USERNAME"
+git config --global user.email "$COMMIT_EMAIL"
 
-REPO_URL="ssh://aur@aur.archlinux.org/${REPO_NAME}.git"
+REPO_URL="ssh://aur@aur.archlinux.org/${PACKAGE_NAME}.git"
 
 echo "---------------- $REPO_URL ----------------"
 
-git clone $REPO_URL
-cd $REPO_NAME
+git clone "$REPO_URL"
+cd "$PACKAGE_NAME"
 
-echo "------------- BUILDING PKG $REPO_NAME ----------------"
+echo "------------- BUILDING PKG $PACKAGE_NAME ----------------"
 
 sed -i "s/pkgver=.*$/pkgver=$NEW_RELEASE/" PKGBUILD
 sed -i "s/sha256sums=.*$/$(makepkg -g 2>/dev/null)/" PKGBUILD
