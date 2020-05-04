581c2cd0485bff19a7e506d8ef05f44bd8549eab
khai96_ <hvksmr1996@gmail.com>
2020-05-04 19:01:26 +0700

    Modify

--- README.md
+++ README.md
@@ -1,36 +1,33 @@
-# AUR publish docker action
+# Publish AUR packages
 
 This action can publish an AUR package.
 
-
-## Requirements
-
-It depends on an environment variable called `GITHUB_REF` to be present and it should also contain a semantic version
-in the format `v0.0.0` to work properly.
-
-This tag should also comply with rules established to publish versions on AUR repository.
-
-You should add to your secrets an SSH private key that match your key uploaded to your AUR account,
-so this action can commit and push to it.
-
-
 ## Inputs
 
-### `package_name`
+### `pkgname`
+
 **Required** The AUR package name you want to update.
 
+### `pkgver`
+
+**Required** The AUR package version you want to update.
+
 ### `commit_username`
+
 **Required** The username to use when creating the new commit.
 
 ### `commit_email`
+
 **Required** The email to use when creating the new commit.
 
 ### `ssh_private_key`
+
 **Required** Your private key with access to AUR package.
 
 
 ## Example usage
-```
+
+```yaml
 name: aur-publish
 
 on:
@@ -43,10 +40,14 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - name: Publish AUR package
-        uses: guumaster/aur-publish-docker-action@v1
+        uses: KSXGitHub/github-actions-deploy-aur@master
         with:
           package_name: my-awesome-package
           commit_username: 'Github Action Bot'
           commit_email: github-action-bot@example.com
           ssh_private_key: ${{ secrets.AUR_SSH_PRIVATE_KEY }}
 ```
+
+## Thanks
+
+This repository is a fork of https://github.com/guumaster/aur-publish-docker-action.git
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,51 +2,50 @@
 
 set -o errexit -o pipefail -o nounset
 
-PACKAGE_NAME=$INPUT_PACKAGE_NAME
+PKGNAME=$INPUT_PKGNAME
+PKGVER=${INPUT_PKGVER}
 COMMIT_USERNAME=$INPUT_COMMIT_USERNAME
 COMMIT_EMAIL=$INPUT_COMMIT_EMAIL
 SSH_PRIVATE_KEY=$INPUT_SSH_PRIVATE_KEY
 
-NEW_RELEASE=${GITHUB_REF##*/v}
-
 export HOME=/home/builder
 
-echo "---------------- AUR Package version $PACKAGE_NAME/$NEW_RELEASE ----------------"
+echo "---------------- AUR Package version $PKGNAME/$PKGVER ----------------"
 
 ssh-keyscan -t ed25519 aur.archlinux.org >> $HOME/.ssh/known_hosts
 
 echo -e "${SSH_PRIVATE_KEY//_/\\n}" > $HOME/.ssh/aur
 
-chmod 600 $HOME/.ssh/aur*
+chmod 600 $HOME/.ssh/aur* || exit $?
 
 git config --global user.name "$COMMIT_USERNAME"
 git config --global user.email "$COMMIT_EMAIL"
 
-REPO_URL="ssh://aur@aur.archlinux.org/${PACKAGE_NAME}.git"
+REPO_URL="ssh://aur@aur.archlinux.org/${PKGNAME}.git"
 
 echo "---------------- $REPO_URL ----------------"
 
 cd /tmp
-git clone "$REPO_URL"
-cd "$PACKAGE_NAME"
+git clone "$REPO_URL" || exit $?
+cd "$PKGNAME" || exit $?
 
-echo "------------- BUILDING PKG $PACKAGE_NAME ----------------"
+echo "------------- BUILDING PKG $PKGNAME ----------------"
 
-sed -i "s/pkgver=.*$/pkgver=$NEW_RELEASE/" PKGBUILD
+sed -i "s/pkgver=.*$/pkgver=$PKGVER/" PKGBUILD
 sed -i "s/sha256sums=.*$/$(makepkg -g 2>/dev/null)/" PKGBUILD
 
 # Test build
-makepkg -c
+makepkg -c || exit $?
 
 # Update srcinfo
-makepkg --printsrcinfo > .SRCINFO
+makepkg --printsrcinfo > .SRCINFO || exit $?
 
 
 echo "------------- BUILD DONE ----------------"
 
 # Update aur
-git add PKGBUILD .SRCINFO
-git commit --allow-empty  -m "Update to $NEW_RELEASE"
-git push
+git add PKGBUILD .SRCINFO || exit $?
+git commit --allow-empty  -m "Update to $PKGVER" || exit $?
+git push --force || exit $?
 
 echo "------------- PUBLISH DONE ----------------"
