fd344922c0fc130ba28fc8ec9864d2c27948678b
khai96_ <hvksmr1996@gmail.com>
2020-05-05 16:59:39 +0700

    Try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "::warning ::inputs.filename = $INPUT_FILENAME"
 echo "::warning ::GITHUB_WORKSPACE = $GITHUB_WORKSPACE"
 
 echo "Content of $(pwd)"
-ls -AR .
+ls .
 
 echo "Content of $INPUT_FILENAME"
-cat the_input_file/input-file.txt
+cat "$INPUT_FILENAME"
