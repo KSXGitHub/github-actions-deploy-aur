628ed630bbb7d0e472a0450e29d000280d3fa9c4
khai96_ <hvksmr1996@gmail.com>
2020-05-05 16:57:15 +0700

    Update

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM archlinux/base
 
 COPY entrypoint.sh /entrypoint.sh
 
-COPY "$INPUT_FILENAME" /input_file
+COPY "$INPUT_FILENAME" the_input_file
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,8 @@ set -o errexit -o pipefail -o nounset
 echo "::warning ::inputs.filename = $INPUT_FILENAME"
 echo "::warning ::GITHUB_WORKSPACE = $GITHUB_WORKSPACE"
 
-echo "Displaying content of $INPUT_FILENAME"
-cat /input_file
+echo "Content of $(pwd)"
+ls -AR .
+
+echo "Content of $INPUT_FILENAME"
+cat the_input_file/input-file.txt
