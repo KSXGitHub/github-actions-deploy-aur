fc2018368350ccf37b8c9e2c62eb462b79860a68
khai96_ <hvksmr1996@gmail.com>
2020-05-05 16:38:20 +0700

    Prepare

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,7 @@
 FROM archlinux/base
 
-RUN pacman -Sy && \
-    pacman -Sy --noconfirm openssh \
-      git fakeroot binutils go-pie gcc awk binutils xz \
-      libarchive bzip2 coreutils file findutils \
-      gettext grep gzip sed ncurses
-
-RUN useradd -ms /bin/bash builder && \
-    mkdir -p /home/builder/.ssh && \
-    touch /home/builder/.ssh/known_hosts
-
-COPY ssh_config /home/builder/.ssh/config
-
-RUN chown builder:builder /home/builder -R && \
-    chmod 600 /home/builder/.ssh/* -R
-
 COPY entrypoint.sh /entrypoint.sh
 
-USER builder
-WORKDIR /home/builder
+COPY "$INPUT_FILENAME" /input_file
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,50 +2,8 @@
 
 set -o errexit -o pipefail -o nounset
 
-PKGNAME=$INPUT_PKGNAME
-PKGVER=${INPUT_PKGVER}
-COMMIT_USERNAME=$INPUT_COMMIT_USERNAME
-COMMIT_EMAIL=$INPUT_COMMIT_EMAIL
-SSH_PRIVATE_KEY=$INPUT_SSH_PRIVATE_KEY
+echo "::warning ::inputs.filename = $INPUT_FILENAME"
+echo "::warning ::GITHUB_WORKSPACE = $GITHUB_WORKSPACE"
 
-export HOME=/home/builder
-
-echo "---------------- AUR Package version $PKGNAME/$PKGVER ----------------"
-
-ssh-keyscan -t ed25519 aur.archlinux.org >> $HOME/.ssh/known_hosts
-
-echo -e "${SSH_PRIVATE_KEY//_/\\n}" > $HOME/.ssh/aur
-
-chmod 600 $HOME/.ssh/aur* || exit $?
-
-git config --global user.name "$COMMIT_USERNAME"
-git config --global user.email "$COMMIT_EMAIL"
-
-REPO_URL="ssh://aur@aur.archlinux.org/${PKGNAME}.git"
-
-echo "---------------- $REPO_URL ----------------"
-
-cd /tmp
-git clone "$REPO_URL" || exit $?
-cd "$PKGNAME" || exit $?
-
-echo "------------- BUILDING PKG $PKGNAME ----------------"
-
-sed -i "s/pkgver=.*$/pkgver=$PKGVER/" PKGBUILD
-sed -i "s/sha256sums=.*$/$(makepkg -g 2>/dev/null)/" PKGBUILD
-
-# Test build
-makepkg -c || exit $?
-
-# Update srcinfo
-makepkg --printsrcinfo > .SRCINFO || exit $?
-
-
-echo "------------- BUILD DONE ----------------"
-
-# Update aur
-git add --force PKGBUILD .SRCINFO || exit $?
-git commit --allow-empty  -m "Update to $PKGVER" || exit $?
-git push --force || exit $?
-
-echo "------------- PUBLISH DONE ----------------"
+echo "Displaying content of $INPUT_FILENAME"
+cat /input_file
