858c6c3c68c7d59f5a316cb21701b0925ba74087
khai96_ <hvksmr1996@gmail.com>
2020-05-06 19:13:17 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,44 +2,32 @@
 
 set -o errexit -o pipefail -o nounset
 
-PKGNAME=$INPUT_PKGNAME
-PKGVER=${INPUT_PKGVER}
-COMMIT_USERNAME=$INPUT_COMMIT_USERNAME
-COMMIT_EMAIL=$INPUT_COMMIT_EMAIL
-SSH_PRIVATE_KEY=$INPUT_SSH_PRIVATE_KEY
-
-echo "---------------- AUR Package version $PKGNAME/$PKGVER ----------------"
+pkgname=$INPUT_PKGNAME
+pkgbuild=$INPUT_PKGBUILD
+commit_username=$INPUT_COMMIT_USERNAME
+commit_email=$INPUT_COMMIT_EMAIL
+ssh_private_key=$INPUT_SSH_PRIVATE_KEY
+commit_message=$INPUT_COMMIT_MESSAGE
 
 ssh-keyscan -t ed25519 aur.archlinux.org >> ~/.ssh/known_hosts
 
-echo -e "${SSH_PRIVATE_KEY//_/\\n}" > ~/.ssh/aur
+echo -e "${ssh_private_key//_/\\n}" > ~/.ssh/aur
 
 chmod 600 ~/.ssh/aur*
 
-git config --global user.name "$COMMIT_USERNAME"
-git config --global user.email "$COMMIT_EMAIL"
+git config --global user.name "$commit_username"
+git config --global user.email "$commit_email"
 
-REPO_URL="ssh://aur@aur.archlinux.org/${PKGNAME}.git"
+repo_url="ssh://aur@aur.archlinux.org/${pkgname}.git"
 
-echo "---------------- $REPO_URL ----------------"
+git clone "$repo_url" /local-repo
 
-cd /tmp
-git clone "$REPO_URL"
-cd "$PKGNAME"
+echo "Copying PKGBUILD from $pkgbuild to /local-repo"
+cp -v "$pkgbuild" /local-repo/PKGBUILD
 
-echo "------------- BUILDING PKG $PKGNAME ----------------"
+echo "Updating .SRCINFO"
+makepkg --printsrcinfo > /local-repo/.SRCINFO
 
-sed -i "s/pkgver=.*$/pkgver=$PKGVER/" PKGBUILD
-sed -i "s/sha256sums=.*$/$(makepkg -g 2>/dev/null)/" PKGBUILD
-
-# Update srcinfo
-makepkg --printsrcinfo > .SRCINFO
-
-echo "------------- BUILD DONE ----------------"
-
-# Update aur
-git add --force PKGBUILD .SRCINFO
-git commit --allow-empty  -m "Update to $PKGVER"
+git add -fv PKGBUILD .SRCINFO
+git commit --allow-empty -m "$commit_message"
 git push --force
-
-echo "------------- PUBLISH DONE ----------------"
