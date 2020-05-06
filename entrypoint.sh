bacc593de812b9e43dc96cbed6faa4caf2eb7b1c
khai96_ <hvksmr1996@gmail.com>
2020-05-06 21:13:14 +0700

    Use -f option

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,7 @@ cp -v /ssh_config ~/.ssh/config
 chmod -v 600 ~/.ssh/*
 
 echo 'Adding aur.archlinux.org to known hosts...'
-ssh-keygen -v -R aur.archlinux.org
-ssh-keyscan -v -t "$ssh_keyscan_types" aur.archlinux.org >> ~/.ssh/known_hosts
+ssh-keyscan -v -t "$ssh_keyscan_types" aur.archlinux.org -f ~/.ssh/known_hosts
 
 echo 'Importing private key...'
 echo "$ssh_private_key" > ~/.ssh/aur
