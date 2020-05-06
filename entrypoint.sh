8f3ff19b5e79dad0a404dfba1071b5ca7a34309f
khai96_ <hvksmr1996@gmail.com>
2020-05-06 19:31:17 +0700

    TEST_LINE_COUNT

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,10 @@ ls .
 echo "Content of $INPUT_FILENAME"
 cat "$INPUT_FILENAME"
 
+echo "Number of lines of inputs.paragraph"
+echo "$INPUT_PARAGRAPH" | wc -l
+
 echo "Content of inputs.paragraph"
-echo "$INPUT_PARAGRAPH"
+echo "$INPUT_PARAGRAPH" | while read -r line; do
+  echo 'line>' "$line"
+done
