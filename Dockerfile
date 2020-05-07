33d52c93994c01db34aa642e59896104f669149c
khai96_ <hvksmr1996@gmail.com>
2020-05-07 11:10:59 +0700

    Split user

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,10 @@ RUN pacman -Sy && \
     pacman -Sy --noconfirm openssh sudo \
       git fakeroot binutils gcc awk binutils xz \
       libarchive bzip2 coreutils file findutils \
-      gettext grep gzip sed ncurses
+      gettext grep gzip sed ncurses util-linux
 
 COPY entrypoint.sh /entrypoint.sh
+COPY build.sh /build.sh
 COPY ssh_config /ssh_config
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,45 +1,21 @@
 #!/bin/bash
-# shellcheck disable=SC2024
 
 set -o errexit -o pipefail -o nounset
 
-pkgname=$INPUT_PKGNAME
 pkgbuild=$INPUT_PKGBUILD
-commit_username=$INPUT_COMMIT_USERNAME
-commit_email=$INPUT_COMMIT_EMAIL
-ssh_private_key=$INPUT_SSH_PRIVATE_KEY
-commit_message=$INPUT_COMMIT_MESSAGE
-ssh_keyscan_types=$INPUT_SSH_KEYSCAN_TYPES
+
+echo 'Creating builder user...'
+useradd --create-home --shell /bin/bash builder
 
 echo 'Initializing ssh directory...'
-mkdir -pv ~/.ssh
-touch ~/.ssh/known_hosts
-cp -v /ssh_config ~/.ssh/config
-chmod -v 600 ~/.ssh/*
+mkdir -pv /home/builder/.ssh
+touch /home/builder/.ssh/known_hosts
+cp -v /ssh_config /home/builder/.ssh/config
+chown -vR builder:builder /home/builder
+chmod -vR 600 ~/.ssh/*
 
-echo 'Adding aur.archlinux.org to known hosts...'
-ssh-keyscan -v -t "$ssh_keyscan_types" aur.archlinux.org >> ~/.ssh/known_hosts
+echo 'Copying PKGBUILD...'
+cp -r "$pkgbuild" /PKGBUILD
 
-echo 'Importing private key...'
-echo "$ssh_private_key" > ~/.ssh/aur
-chmod 600 ~/.ssh/aur*
-
-echo 'Configuring git...'
-git config --global user.name "$commit_username"
-git config --global user.email "$commit_email"
-
-echo "Cloning AUR package into /local-repo..."
-git clone "https://aur.archlinux.org/${pkgname}.git" /local-repo
-
-echo "Copying PKGBUILD from $pkgbuild to /local-repo"
-cp -v "$pkgbuild" /local-repo/PKGBUILD
-
-echo "Updating .SRCINFO"
-sudo makepkg --printsrcinfo > /local-repo/.SRCINFO
-
-echo "Publishing..."
-cd /local-repo
-git remote add aur "ssh://aur@aur.archlinux.org/${pkgname}.git"
-git add -fv PKGBUILD .SRCINFO
-git commit --allow-empty -m "$commit_message"
-git push -fv aur master
+echo 'Running build.sh...'
+exec runuser builder --login --command '/build.sh'
